c1494ac5771c3bf687898de53b0d390911ceee14
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-11 06:05:39 +0000

    [Finish]assignment_8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: "passive_relationships", source: "follower"
+  
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
